319243fc3585838b377f104f0101a6761d4f2351
Raz Marshanski <razmarshanski@gmail.com>
2022-03-18 00:00:07 +0200

    add a new folder

---
+++ c++ course/Section 13 - OOP classes and Objects/declareClassAndObject.cpp
@@ -0,0 +1,38 @@
+/********************************************************
+ * Author : Raz Marshanski
+ * Date   :
+ * Name   : pointers
+*******************************************************/
+//-----------------------INCLUDE-----------------------
+using namespace std;
+#include <iostream>
+#include <string>
+#include <vector>
+//-----------------------------------------------------
+//-----------------------CONSTANTS---------------------
+//-----------------------------------------------------
+//-------------------FUNCTION-PROTOTYPE----------------
+//-----------------------------------------------------
+class player
+{
+    //attributes
+    string name;
+    int health;
+    int xp;
+
+    //methods
+    void talk(string);
+    bool is_dead();
+};
+
+
+int main()
+{
+    player frank;
+    player hero;
+
+    return 0;
+    
+} 
+
+
