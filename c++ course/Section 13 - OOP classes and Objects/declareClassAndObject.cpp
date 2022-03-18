e9b4c532e76fcba04a64949e7b068ee9a960db49
Raz Marshanski <razmarshanski@gmail.com>
2022-03-18 17:41:57 +0200

    add some code according to the video

--- c++ course/Section 13 - OOP classes and Objects/declareClassAndObject.cpp
+++ c++ course/Section 13 - OOP classes and Objects/declareClassAndObject.cpp
@@ -15,21 +15,52 @@ using namespace std;
 //-----------------------------------------------------
 class player
 {
+public:
     //attributes
     string name;
     int health;
     int xp;
 
     //methods
-    void talk(string);
+    void talk(string text_to_say){cout << name << " says " << text_to_say << endl; };
     bool is_dead();
+};
+
+class Account
+{
+public:
+    //attributes
+    string name;
+    int balance;
+
+    //methods
+    bool deposit (double ball){balance += ball; cout << "In deposit" << endl;};
+    bool withdraw(double ball){balance -= ball; cout << "In withdrew" << endl;};
 };
 
 
 int main()
 {
+    Account frank_account;
+    frank_account.name    = "Frank's account";
+    frank_account.balance = 5000.0;
+
+    frank_account.deposit(1000.0);
+    frank_account.withdraw(500.0);
+
+
     player frank;
-    player hero;
+    frank.name   = "Frank";
+    frank.health = 100;
+    frank.xp     = 12;
+    frank.talk("Hi there");
+
+    player *enemy   = new player;
+    (*enemy).name   = "Enemy";
+    (*enemy).health = 100;
+    enemy->xp       = 15;
+
+    enemy->talk("I will destroy you");
 
     return 0;
     
